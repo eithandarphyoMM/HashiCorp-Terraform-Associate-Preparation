5125a0afbda183d7dffb0a31e01e9399348dcbbb
eithandarphyoMM <eithandar118@gmail.com>
2026-07-28 15:10:32 +0000

    switch to Ubuntu, add NGINX user_data, and egress rule_20260728

--- 08-exercise-2026-Jul-26/README.md
+++ 08-exercise-2026-Jul-26/README.md
@@ -4,5 +4,5 @@ Steps for implemenation:
 3. 「DONE」Setup a route table with a route to the IGW
 4. 「DONE」 Deploy an EC2 instance inside of the created subnet and associate a public IP
 5. 「DONE」 Associate a security group that allows public ingress
-6. Change the EC2 instance to use a publicly available NGINX AMI
+6. 「DONE」 Change the EC2 instance to use a publicly available NGINX AMI
 7. Destroy everything
--- 08-exercise-2026-Jul-26/compute.tf
+++ 08-exercise-2026-Jul-26/compute.tf
@@ -1,17 +1,25 @@
-# Fetch the latest x86_64 Amazon Linux 2023 AMI ID dynamically
-data "aws_ssm_parameter" "al2023_x86" {
-  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+# Fetch the latest x86_64 Ubuntu 24.04 LTS AMI dynamically via SSM
+data "aws_ssm_parameter" "ubuntu_2404_x86" {
+  name = "/aws/service/canonical/ubuntu/server/24.04/stable/current/amd64/hvm/ebs-gp3/ami-id"
 }
 
 resource "aws_instance" "web" {
-  ami                         = data.aws_ssm_parameter.al2023_x86.value
-  associate_public_ip_address = true
+  user_data = <<-EOF
+            #!/bin/bash
+            apt-get update -y
+            apt-get install -y nginx
+            systemctl enable nginx
+            systemctl start nginx
+            EOF
+
+  ami                         = data.aws_ssm_parameter.ubuntu_2404_x86.value
   instance_type               = "t3.micro"
+  associate_public_ip_address = true
 
   root_block_device {
     delete_on_termination = true
     volume_size           = 8
-    volume_type           = "gp2"
+    volume_type           = "gp3"
   }
 
   subnet_id = aws_subnet.public.id
@@ -19,7 +27,7 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.public_http_traffic.id]
 
   tags = merge(local.common_tags, {
-    Name = "08-exercise-2026-Jul-26-web-instance"
+    Name = "08-exercise-2026-Jul-26-Ubuntu-WebServer"
   })
 }
 
@@ -44,3 +52,10 @@ resource "aws_vpc_security_group_ingress_rule" "https" {
   to_port           = 443
   ip_protocol       = "tcp"
 }
+
+resource "aws_vpc_security_group_egress_rule" "all" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
