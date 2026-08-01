93e29b427caa904ec334d333899cfea6e64466b8
eithandarphyoMM <eithandar118@gmail.com>
2026-08-01 12:40:32 +0000

    Working with .auto.tfvars files_20260801

---
+++ 11-2026-Aug-01-input-vars-locals-outputs/terraform.tfvars
@@ -0,0 +1,6 @@
+aws_region        = "ap-southeast-1"
+ec2_instance_type = "t4g.micro"
+ec2_volume_config = {
+  size = "20"
+  type = "gp2"
+}
--- 11-2026-Aug-01-input-vars-locals-outputs/variables.tf
+++ 11-2026-Aug-01-input-vars-locals-outputs/variables.tf
@@ -10,8 +10,8 @@ variable "ec2_instance_type" {
   default     = "t3.micro"
 
   validation {
-    condition     = contains(["t3.micro", "t3.small", "t4g.micro"], var.ec2_instance_type) //Free Tier eligible instance types
-    error_message = "Invalid EC2 instance type. Allowed values are: t3.micro, t3.small, t4g.micro."
+    condition     = contains(["t3.micro", "t3.small", "t4g.micro", "t4g.small"], var.ec2_instance_type) //Free Tier eligible instance types
+    error_message = "Invalid EC2 instance type. Allowed values are: t3.micro, t3.small, t4g.micro, t4g.small."
   }
 }
 
