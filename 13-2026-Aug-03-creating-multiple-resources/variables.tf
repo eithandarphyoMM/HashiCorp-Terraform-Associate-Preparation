1827e5c9994d40cac8c0db69c2e31eba1d30a938
eithandarphyoMM <eithandar118@gmail.com>
2026-08-03 14:19:55 +0000

    referencing resources with count meta-argument_20260803

--- 13-2026-Aug-03-creating-multiple-resources/variables.tf
+++ 13-2026-Aug-03-creating-multiple-resources/variables.tf
@@ -2,7 +2,28 @@ locals {
   project = "my-aws-project"
 }
 
-variable "subnet_count" {
+variable "ec2_instance_count" {
   type    = number
-  default = 3
+  default = 1
+}
+
+variable "aws_subnet" {
+  type = list(object({
+    cidr_block        = string
+    availability_zone = string
+  }))
+  default = [
+    {
+      cidr_block        = "10.0.1.0/24"
+      availability_zone = "ap-northeast-1a"
+    },
+    {
+      cidr_block        = "10.0.2.0/24"
+      availability_zone = "ap-northeast-1b"
+    },
+    {
+      cidr_block        = "10.0.3.0/24"
+      availability_zone = "ap-northeast-1c"
+    }
+  ]
 }
