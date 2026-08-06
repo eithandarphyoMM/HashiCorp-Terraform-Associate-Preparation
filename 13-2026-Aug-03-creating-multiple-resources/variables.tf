29751609cf621ebe546bcda2ee91309c2fafe735
eithandarphyoMM <eithandar118@gmail.com>
2026-08-06 14:46:32 +0000

    add validation into a map variable _20260806

--- 13-2026-Aug-03-creating-multiple-resources/variables.tf
+++ 13-2026-Aug-03-creating-multiple-resources/variables.tf
@@ -1,8 +1,27 @@
-locals {
-  project = "my-aws-project"
+variable "subnet_count" {
+  type        = number
+  default     = 3
+  description = "Number of subnets to create in ap-northeast-1"
 }
 
-variable "subnet_count" {
-  type    = number
-  default = 3
+variable "ec2_instance_config_map" {
+  type = map(object({
+    instance_type = string
+    ami           = string
+    subnet_name   = optional(string, "default")
+  }))
+
+  validation {
+    condition = alltrue([
+      for config in values(var.ec2_instance_config_map) : contains(["t3.micro"], config.instance_type)
+    ])
+    error_message = "Only t3.micro instances are allowed for Free Tier in ap-northeast-1."
+  }
+
+  validation {
+    condition = alltrue([
+      for config in values(var.ec2_instance_config_map) : contains(["nginx", "ubuntu"], config.ami)
+    ])
+    error_message = "At least one of the provided \"ami\" values is not supported.\nSupported \"ami\" values: \"ubuntu\", \"nginx\"."
+  }
 }
